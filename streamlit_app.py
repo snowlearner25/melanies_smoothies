497cbaa72ca27154cb3c84edb5dd6e6f676e3286
snowlearner25 <wzhang12_2000@yahoo.com>
2025-03-25 00:58:59 -0400

    Update streamlit_app.py
    update use_container_width

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ if time_to_insert:
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
+sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
